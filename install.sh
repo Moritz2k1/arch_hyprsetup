66513280d800ce98a64eca01c8d9cb70219381da
Moritz2k1 <moritz.gutschi@outlook.com>
2024-12-13 23:33:41 +0100

    Added more

--- install.sh
+++ install.sh
@@ -2,4 +2,24 @@
 
 echo "Updating System"
 wait(3)
-sudo pacman -Syu git kitty hyprland thunar neovim npm nodejs firefox kvantum lazygit ark
+sudo pacman -Syu
+
+echo "Installing core packages"
+wait(3)
+sudo pacman -S git kitty thunar npm nodejs jre-openjdk jdk-openjdk neovim lazygit tmux curl
+
+echo "Installing AUR Helper yay"
+wait(3)
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -s
+cd
+rm -rf yay
+
+echo "Installing additional programs"
+wait(3)
+yay -S firefox ark kvantum nwg-look 
+
+
+echo "Installing hyprland"
+yay -S hyprland
